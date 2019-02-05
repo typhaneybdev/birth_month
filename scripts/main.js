cfad6f5693d57035094f6d3f9d7e8612680c79c6
Typhaney-Shanker <typhaney@gmail.com>
2019-02-04 22:05:00 -0700

    input with basic switch

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,49 @@
-
 //getting form input value
 function monthInput() {
   let month = document.getElementById("month").value;
+  event.preventDefault();
+  switch (month) {
+    case "January":
+      text = "garnet is your birthstone";
+      break;
+    case "February":
+      text = "Amethyst is your birthstone";
+      break;
+    case "March":
+      text = "Aquamarine is your birthstone";
+      break;
+    case "April":
+      text = "Diamond is your birthstone";
+      break;
+    case "May":
+      text = "Emerald is your birthstone";
+      break;
+    case "June":
+      text = "Alexandrite & Pearl are your birthstones";
+      break;
+    case "July":
+      text = "Ruby is your birthstone";
+      break;
+    case "August":
+      text = "Peridot is your birthstone";
+      break;
+    case "September":
+      text = "Sapphire is your birthstone";
+      break;
+    case "October":
+      text = "Opal & Tourmaline are your birthstones";
+      break;
+    case "November":
+      text = "Citrine & Topaz are your birthstones";
+      break;
+    case "December":
+      text = "Tanzanite, Turquoise and Blue Zircon are your birthstones";
+      break;
+
+    default:
+      "this is a test";
+  }
+  document.getElementById("test").innerHTML = text;
 
   console.log(month);
 }
